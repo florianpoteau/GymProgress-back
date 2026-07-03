74c43ce4c42ca280ab5619433f31c5b8083756e7
Poteau Florian <florianpoteau59@gmail.com>
2026-07-03 13:46:36 +0200

    feat(exercise prescription): add exercise prescription class with id, exercice and difficulty level

---
+++ src/main/java/com/gymprogress/gymprogress/persistance/entity/ExercisesPrescription.java
@@ -0,0 +1,33 @@
+package com.gymprogress.gymprogress.persistance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "exercise_prescriptions")
+public class ExercisesPrescription {
+    
+    @Id
+    @Column(name = "exercise_prescription_id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    
+    @ManyToOne
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+
+    @ManyToOne
+    @JoinColumn(name = "difficulty_levels_id", nullable = false)
+    private DifficultyLevels difficultyLevels;
+
+}
