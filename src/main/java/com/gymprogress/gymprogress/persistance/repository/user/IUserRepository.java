5f45bb75c5afa8c589608aaec2d00932f4b014a8
Poteau Florian <florianpoteau59@gmail.com>
2026-07-03 15:41:33 +0200

    feat(user): add query for get account by email

--- src/main/java/com/gymprogress/gymprogress/persistance/repository/user/IUserRepository.java
+++ src/main/java/com/gymprogress/gymprogress/persistance/repository/user/IUserRepository.java
@@ -1,11 +1,17 @@
 package com.gymprogress.gymprogress.persistance.repository.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gymprogress.gymprogress.persistance.entity.User;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
+
+    @Query(UserQueries.FIND_USER_ACCOUNT_BY_EMAIL)
+    Optional<User> findUserAccountByEmail(String Email);
 
 }
---
+++ src/main/java/com/gymprogress/gymprogress/persistance/repository/user/UserQueries.java
@@ -0,0 +1,8 @@
+package com.gymprogress.gymprogress.persistance.repository.user;
+
+public final class UserQueries {
+
+    public static final String FIND_USER_ACCOUNT_BY_EMAIL =
+      "SELECT u FROM User u WHERE u.userEmail = :userEmail";
+
+}
