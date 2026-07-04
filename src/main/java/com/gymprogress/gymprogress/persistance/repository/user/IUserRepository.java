7b45fc159a2545c03c167b1382c5c73362678504
Poteau Florian <florianpoteau59@gmail.com>
2026-07-04 15:48:05 +0200

    fix(user): add @Param to find user by email query

--- src/main/java/com/gymprogress/gymprogress/persistance/repository/user/IUserRepository.java
+++ src/main/java/com/gymprogress/gymprogress/persistance/repository/user/IUserRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.gymprogress.gymprogress.persistance.entity.User;
@@ -12,6 +13,6 @@ import com.gymprogress.gymprogress.persistance.entity.User;
 public interface IUserRepository extends JpaRepository<User, Integer> {
 
     @Query(UserQueries.FIND_USER_ACCOUNT_BY_EMAIL)
-    Optional<User> findUserAccountByEmail(String Email);
+    Optional<User> findUserAccountByEmail(@Param("email") String email);
 
 }
--- src/main/java/com/gymprogress/gymprogress/persistance/repository/user/UserQueries.java
+++ src/main/java/com/gymprogress/gymprogress/persistance/repository/user/UserQueries.java
@@ -3,6 +3,6 @@ package com.gymprogress.gymprogress.persistance.repository.user;
 public final class UserQueries {
 
     public static final String FIND_USER_ACCOUNT_BY_EMAIL =
-      "SELECT u FROM User u WHERE u.userEmail = :userEmail";
+      "SELECT u FROM User u WHERE u.userEmail = :email";
 
 }
